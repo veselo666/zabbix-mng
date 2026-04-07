b7b44cc82d350bde17dea9943e81dc88978d51f6
veselo666 <pentinam@gmail.com>
2026-04-07 16:31:46 +0800

    Update README.sh

--- README.sh
+++ README.sh
@@ -1,10 +1,1 @@
-# Media Types
-MEDIA_TYPE_EMAIL=Email
-LDAP_EMAIL_ATTR=mail
-DEFAULT_MEDIA_TYPE=Email
 
-# Optional: custom patterns for media assignment
-# CSC_MEDIA_EMAIL=cr-gd-zabbix.*csc.*
-# ATS_MEDIA_EMAIL=cr-gd-zabbix.*ats.*
-# FCS_MEDIA_EMAIL=cr-gd-zabbix.*fcs.*
-# JET_MEDIA_EMAIL=cr-gd-zabbix.*jet.*
